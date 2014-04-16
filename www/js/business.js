2b13059cb05565d96855fef6eb540b53acd41666
Didier <didier.lefevre@gmail.com>
2014-04-16 18:48:55 +0200

    déport urls

--- www/js/business.js
+++ www/js/business.js
@@ -16,8 +16,6 @@ var Serie = function (item) {
     }
 };
 
-var topserieUrl = "http://192.168.1.19/webapp/EverStreamProxy.ashx";
-var getEpisodesURL = "http://192.168.1.19/webapp/EverStreamProxy.ashx?GetEpisodes&serieId={id}";
 var EverStreamViewModel = function () {
     var self = this;
 
---
+++ www/js/server.js
@@ -0,0 +1,4 @@
+var topserieUrl = 
+
+	"http://192.168.1.19/webapp/EverStreamProxy.ashx";
+var getEpisodesURL = "http://192.168.1.19/webapp/EverStreamProxy.ashx?GetEpisodes&serieId={id}";
