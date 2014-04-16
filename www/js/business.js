681a3729e40caa7d9fba8dcbeb05b69970ef5d8e
didier <didier.lefevre@gmail.com>
2014-04-16 23:34:05 +0200

    sélecteur épisodes

--- www/js/business.js
+++ www/js/business.js
@@ -54,7 +54,25 @@ var EverStreamViewModel = function () {
 
     this.currentSerieSeasons = ko.observableArray([]);
 
+    this.currentSeasonEpisodes = ko.observableArray([]);
+    this.currentSeasonEpisodesIdx = ko.observable(-1);
+
     this.currentSerieHistory = ko.observable({season: '-'});
+
+    this.currentSerieHistory.subscribe( function( selectedSeason )
+    {
+       // extraire la liste des épisodes disponibles pour cette saison
+        self.currentSeasonEpisodesIdx(-1);
+        self.currentSeasonEpisodes([]);
+        $.each( self.episodes(), function( index, item ) {
+           if( item.season == selectedSeason.season )
+           {
+               self.currentSeasonEpisodes.push( item );
+           }
+        });
+        if( self.currentSeasonEpisodes().length > 0 )
+            self.currentSeasonEpisodesIdx(0);
+    });
 
     // chargement de la liste des top séries
     // depuis le serveur ou depuis le localstorage
@@ -149,7 +167,10 @@ var EverStreamViewModel = function () {
         }
         self.currentSerieHistory( item );
     };
-	
+
+    //----------------------------------
+    // Sélection de la saison suivante
+    //----------------------------------
 	this.seasonNext = function()
 	{
 		console.log( 'next');
@@ -165,6 +186,24 @@ var EverStreamViewModel = function () {
 			self.currentSerieHistory( item );	
 		}
 	}
+    //------------------------------------
+    // Sélection de la saison précédente
+    //------------------------------------
+    this.seasonPrev = function()
+    {
+        console.log( 'prev');
+        var idx = self.currentSerieSeasons.indexOf( self.currentSerieHistory().season );
+        console.log( 'idx=' + idx);
+        if( idx > 0 )
+        {
+            console.log( 'ok' );
+            idx--;
+            var item = {
+                season: self.currentSerieSeasons()[idx]
+            };
+            self.currentSerieHistory( item );
+        }
+    }
 
     this.loadStreams = function (episode) {
 
