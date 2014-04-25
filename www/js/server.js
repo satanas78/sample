dad08a2c429b543a3d56513e1c008dc9213a7da3
didier <didier.lefevre@gmail.com>
2014-04-25 23:18:09 +0200

    ip

--- www/js/server.js
+++ www/js/server.js
@@ -1,3 +1,3 @@
-var topserieUrl = "http://192.168.1.19/webapp/EverStreamProxy.ashx";
-var getEpisodesURL = "http://192.168.1.19/webapp/EverStreamProxy.ashx?GetEpisodes&serieId={id}";
-var getStreamsURL = "http://192.168.1.19/webapp/EverStreamProxy.ashx?GetEpisodeStreams&serieId={id}&season={season}&episode={episode}";
+var topserieUrl = "http://192.168.1.20/webapp/EverStreamProxy.ashx";
+var getEpisodesURL = "http://192.168.1.20/webapp/EverStreamProxy.ashx?GetEpisodes&serieId={id}";
+var getStreamsURL = "http://192.168.1.20/webapp/EverStreamProxy.ashx?GetEpisodeStreams&serieId={id}&season={season}&episode={episode}";
